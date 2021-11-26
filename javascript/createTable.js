02782e3752b22c47bbb82dd5420d5395b16ab0eb
lsyoung <sulyoung7@gmail.com>
2021-11-26 17:40:05 +0900

    ajax

---
+++ javascript/createTable.js
@@ -0,0 +1,46 @@
+//createTable.js => 타이틀 배열, 데이터 배열
+function createTable(titleAry, dataAry) {
+    //tr생성 함수
+    function makeRow(obj) {
+        var tr = document.createElement('tr');
+        titleAry.forEach(field => {
+            var td = document.createElement('td');
+            td.textContent = obj[field];
+            tr.appendChild(td);
+
+        });
+        //추가 컬럼
+        var td = document.createElement('td');
+        td.innerHTML = `<a target ="_blank" href="daummap.html?lat=${obj.lat}&lng=${obj.lng}$cent=${obj.centerName}">지도</a>`;
+        tr.appendChild(td);
+     
+        return tr;
+    }
+    //table작성
+    var table = document.createElement('table');
+    table.setAttribute('border', '1');
+    
+    //thead 작성
+    var thead = document.createElement('thead');
+    var tr = document.createElement('tr');
+    thead.appendChild(tr);
+    titleAry.forEach(item => {
+        var th = document.createElement('th');
+        th.textContent = item;
+        tr.appendChild(th);
+    });
+    //지도 표시
+    var th = document.createElement('th');
+    th.textContent = '지도';
+    tr.appendChild(th);
+
+
+    //tbody작성
+    var tbody = document.createElement('tbody');
+    dataAry.forEach(item => {
+        tbody.appendChild(makeRow(item));
+    });
+    table.append(thead, tbody);
+
+    return table;
+}
